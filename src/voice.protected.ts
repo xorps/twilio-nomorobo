9ca13e101040d2e686a2a3f419ec2e194973171c
xorps <758027+xorps@users.noreply.github.com>
2021-07-21 20:57:34 -0700

    update

--- src/voice.protected.ts
+++ src/voice.protected.ts
@@ -1,20 +1,29 @@
 import '@twilio-labs/serverless-runtime-types';
-import type { ServerlessFunctionSignature } from '@twilio-labs/serverless-runtime-types/types';
+import type { Context, ServerlessFunctionSignature } from '@twilio-labs/serverless-runtime-types/types';
+import VoiceResponse from 'twilio/lib/twiml/VoiceResponse';
 
-export const handler: ServerlessFunctionSignature<{SIM: string}, {From: string}> = async (context, event, callback) => {
-  try {
-    const client = context.getTwilioClient();
-    const twiml = new Twilio.twiml.VoiceResponse();
-    const response = await client.lookups.phoneNumbers(event.From).fetch({addOns: 'nomorobo_spamscore'});
-    const score = response.addOns.results.nomorobo_spamscore.result.score;
-    const block = score == 1;
-    if (block) {
-      twiml.reject();
-    } else {
-      twiml.dial().sim(context.SIM);
-    }
-    callback(null, twiml);
-  } catch (err) {
-    callback(err)
+type Env = {SIM: string};
+type Event = {From: string};
+type Handler = ServerlessFunctionSignature<Env, Event>;
+
+async function run(context: Context<Env>, event: Event): Promise<VoiceResponse> {
+  const client = context.getTwilioClient();
+  const twiml = new Twilio.twiml.VoiceResponse();
+  const response = await client.lookups
+    .phoneNumbers(event.From)
+    .fetch({addOns: 'nomorobo_spamscore'});
+  const score = response.addOns.results.nomorobo_spamscore.result.score;
+  const shouldBlock = score == 1;
+  if (shouldBlock) {
+    twiml.reject();
+  } else {
+    twiml.dial().sim(context.SIM);
   }
+  return twiml;
+}
+
+export const handler: Handler = (context, event, callback) => {
+  run(context, event)
+    .then(twiml => callback(null, twiml))
+    .catch(err => callback(err));
 };
